f1569c95ee9ea3ef3ab3e511d2c042067bd7c816
sherlock <qinglingou@gmail.com>
2025-03-02 19:15:59 +0800

    feat: 增加最大化按钮
    增加最大化按钮

--- src/main/kotlin/com/xigong/xiaozhuan/widget/RootWindow.kt
+++ src/main/kotlin/com/xigong/xiaozhuan/widget/RootWindow.kt
@@ -30,6 +30,8 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
 import com.xigong.xiaozhuan.BuildConfig
 import com.xigong.xiaozhuan.page.splash.SplashPage
 import com.xigong.xiaozhuan.page.version.NewVersionDialog
@@ -38,6 +40,7 @@ import com.xigong.xiaozhuan.style.AppShapes
 
 @Composable
 fun FrameWindowScope.RootWindow(
+    state: WindowState,
     closeClick: () -> Unit,
     content: @Composable () -> Unit
 ) {
@@ -57,7 +60,7 @@ fun FrameWindowScope.RootWindow(
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
-                TopBar(closeClick)
+                TopBar(state, closeClick)
                 content()
             }
             SplashPage()
@@ -68,7 +71,7 @@ fun FrameWindowScope.RootWindow(
 }
 
 @Composable
-private fun FrameWindowScope.TopBar(closeClick: () -> Unit) {
+private fun FrameWindowScope.TopBar(state: WindowState, closeClick: () -> Unit) {
     WindowDraggableArea {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -88,6 +91,13 @@ private fun FrameWindowScope.TopBar(closeClick: () -> Unit) {
             Spacer(modifier = Modifier.weight(1f))
             ImageButton("window_mini.png", 20.dp) {
                 window.isMinimized = true
+            }
+            ImageButton("window_max.png", 20.dp) {
+                if (state.placement == WindowPlacement.Floating) {
+                    state.placement = WindowPlacement.Maximized
+                } else {
+                    state.placement = WindowPlacement.Floating
+                }
             }
             ImageButton("window_close.png", 14.dp, closeClick)
         }
