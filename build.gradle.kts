1e8a365db03dd324d2170c42fa81e73c223d9c72
sherlock <qinglingou@gmail.com>
2025-03-25 15:23:25 +0800

    release: 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.jetbrains.compose")
 }
 
-val appVersion = AppVersion(1, 1, 2)
+val appVersion = AppVersion(2, 0, 0)
 
 @Suppress("SpellCheckingInspection")
 val packageId = "com.xigong.xiaozhuan"
